82452dc61c5e2a40b6637eedb10c8aa2d1e76117
Raul Centeno <raulcenteno3@gmail.com>
2023-04-14 13:50:57 -0600

    chore: try updated logic (#38)
    Co-authored-by: Raul Centeno <raul.centeno@slalom.com>

--- scripts/secureli-deployment.sh
+++ scripts/secureli-deployment.sh
@@ -8,13 +8,15 @@ if git rev-parse "v${secureliVersion}" >/dev/null 2>&1; then
     echo "The tag v${secureliVersion} that would have been deployed already exists, ending pipeline execution..."
     exit 1
 else
+    cd .. #Return back to secureli home dir to retrieve release
     echo "The tag v${secureliVersion} does not exist, proceeding with deployment"
     echo "Pulling down the most recent published secureli release v${secureliVersion}"
     gh release download v$secureliVersion
     export secureliSha256=$(sha256sum ./secureli-${secureliVersion}.tar.gz | awk '{print $1}')
     git config --global user.email "secureli-automation@slalom.com"
     git config --global user.name "Secureli Automation"
-    python ../scripts/get-secureli-dependencies.py
+    cd homebrew-secureli
+    python ./scripts/get-secureli-dependencies.py
     git checkout -b "secureli-${secureliVersion}-formula-generation"
     git add ./Formula/secureli.rb
     git commit -m "Creating pull request with latest Secureli formula for version ${secureliVersion}"
