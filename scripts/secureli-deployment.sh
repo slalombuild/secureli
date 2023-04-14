1412bcc42bef69913b27b430a49e299652da38b2
Raul Centeno <raulcenteno3@gmail.com>
2023-04-14 12:01:28 -0600

    chore: Fix conditional logic (#37)
    Co-authored-by: Raul Centeno <raul.centeno@slalom.com>

--- scripts/secureli-deployment.sh
+++ scripts/secureli-deployment.sh
@@ -3,6 +3,7 @@ set -exo pipefail
 
 pip install requests jinja2 poetry python-semantic-release # TODO: Look into Dockerizing these tools so we're not pulling them down each pipeile run
 export secureliVersion=$(semantic-release print-version --current)
+cd homebrew-secureli
 if git rev-parse "v${secureliVersion}" >/dev/null 2>&1; then
     echo "The tag v${secureliVersion} that would have been deployed already exists, ending pipeline execution..."
     exit 1
@@ -13,8 +14,7 @@ else
     export secureliSha256=$(sha256sum ./secureli-${secureliVersion}.tar.gz | awk '{print $1}')
     git config --global user.email "secureli-automation@slalom.com"
     git config --global user.name "Secureli Automation"
-    python ./scripts/get-secureli-dependencies.py
-    cd homebrew-secureli
+    python ../scripts/get-secureli-dependencies.py
     git checkout -b "secureli-${secureliVersion}-formula-generation"
     git add ./Formula/secureli.rb
     git commit -m "Creating pull request with latest Secureli formula for version ${secureliVersion}"
