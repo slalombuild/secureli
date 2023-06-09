e2327028f6df35e8f7545bdd84308135c53ee3ff
Hartono Ramli <hartonor@slalom.com>
2023-06-09 11:41:35 -0700

    chore: send log through log api (#117)
    This add the functionality to send log through http post.
    Currently seCureLI only support New Relic as the observability platform.
    (or any platform that use the same request format as new relic) e.g:
    ```
    requests.post(
            url=API_ENDPOINT, headers={"Api-Key": API_KEY}, data=log_data
        )
    ```
    
    <img width="1581" alt="Screenshot 2023-06-08 at 8 58 57 AM"
    src="https://github.com/slalombuild/secureli/assets/13907880/b7c592de-7643-4bf9-be9b-855a8d80ae30">
    
    Note:
    Missing unit test that require mocking api-key, due to a bug in ignoring
    test key that cause the inability to commit file

--- secureli/utilities/usage_stats.py
+++ secureli/utilities/usage_stats.py
@@ -1,3 +1,6 @@
+import requests
+import os
+
 from secureli.services.scanner import Failure
 from collections import Counter
 
@@ -15,3 +18,22 @@ def convert_failures_to_failure_count(failure_list: list[Failure]):
 
     failure_count_list = Counter(list_of_failure_ids)
     return failure_count_list
+
+
+def post_log(log_data: str):
+    """
+    Send a log through http post
+    :param log_data: a string to be sent to backend instrumentation
+    """
+
+    API_ENDPOINT = os.getenv("API_ENDPOINT")
+    API_KEY = os.getenv("API_KEY")
+
+    if not API_ENDPOINT or not API_KEY:
+        return
+
+    result = requests.post(
+        url=API_ENDPOINT, headers={"Api-Key": API_KEY}, data=log_data
+    )
+
+    return result.text
--- tests/utilities/test_usage_stats.py
+++ tests/utilities/test_usage_stats.py
@@ -1,5 +1,9 @@
-from secureli.utilities.usage_stats import convert_failures_to_failure_count
+from secureli.utilities.usage_stats import post_log, convert_failures_to_failure_count
 from secureli.services.scanner import Failure
+from unittest import mock
+from unittest.mock import Mock, patch
+
+import os
 
 
 def test_that_convert_failures_to_failure_count_returns_correct_count():
@@ -21,3 +25,35 @@ def test_that_convert_failures_to_failure_count_returns_correctly_when_no_failur
     result = convert_failures_to_failure_count(list_of_failure)
 
     assert result == {}
+
+
+@mock.patch.dict(
+    os.environ, {"API_KEY": "", "API_ENDPOINT": "testendpoint"}, clear=True
+)
+def test_that_post_log_return_none_when_no_api_key():
+    result = post_log("testing")
+
+    assert result == None
+
+
+# pragma: allowlist nextline secret
+@mock.patch.dict(os.environ, {"API_KEY": "testkey", "API_ENDPOINT": ""}, clear=True)
+def test_that_post_log_return_none_when_no_api_endpoint():
+    result = post_log("testing")
+
+    assert result == None
+
+
+@mock.patch.dict(
+    os.environ,
+    {"API_KEY": "testkey", "API_ENDPOINT": "testendpoint"},  # pragma: allowlist secret
+    clear=True,
+)
+@patch("requests.post")
+def test_that_post_log_return_correctly_when_argument_is_correct(mock_requests):
+    mock_requests.return_value = Mock(status_code=202, text={"requestId": "test-0001"})
+
+    result = post_log("test_log_data")
+
+    mock_requests.assert_called_once()
+    assert result == {"requestId": "test-0001"}
