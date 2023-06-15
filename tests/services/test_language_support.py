df4d9164d4a2b8649f6400eb647629abf4b0d482
Ryan Graue <71246823+rgraue@users.noreply.github.com>
2023-06-15 12:37:37 -0700

    feat: Add JS Support (#133)
    js support with eslint + prettier

--- tests/services/test_language_support.py
+++ tests/services/test_language_support.py
@@ -2,16 +2,25 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from secureli.abstractions.pre_commit import InstallResult
+from secureli.abstractions.pre_commit import (
+    InstallResult,
+    LanguagePreCommitConfigInstallResult,
+)
 from secureli.services.language_support import LanguageSupportService
 
 
 @pytest.fixture()
 def mock_pre_commit_hook() -> MagicMock:
+    mock_pre_commit_config_install_result = LanguagePreCommitConfigInstallResult(
+        num_successful=0, num_non_success=0, non_success_messages=list()
+    )
+
     mock_pre_commit_hook = MagicMock()
     mock_pre_commit_hook.version_for_language.return_value = "abc123"
     mock_pre_commit_hook.install.return_value = InstallResult(
-        successful=True, version_installed="abc123"
+        successful=True,
+        version_installed="abc123",
+        configs_result=mock_pre_commit_config_install_result,
     )
     mock_pre_commit_hook.secret_detection_hook_id.return_value = "baddie-finder"
     return mock_pre_commit_hook
